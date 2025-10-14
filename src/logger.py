b90a6a9a37ae00bd200ee944dee5dbfb014e2578
adrien <a.d44@tuta.io>
2025-10-14 21:17:37 +0200

    refactor: Improve logging and standardize device management

--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,5 @@
-"""Configure and initialize the application's logging system."""
-
-import logging.config
 from src.settings import SETTINGS
+import logging.config
 
 logging.config.fileConfig(
     SETTINGS.LOG_CONF_PATH,
@@ -12,4 +10,12 @@ logging.config.fileConfig(
 root_logger = logging.getLogger()
 root_logger.setLevel(SETTINGS.LOG_LEVEL)
 
+
+def newline_module(log_level="DEBUG", how_many_lines=1):
+    if SETTINGS.LOG_LEVEL == log_level:
+        for _ in range(how_many_lines):
+            print("")
+
+
+logging.newline = newline_module
 logger = logging.getLogger(__name__)
