eea1a8439ee5350968a05a3dd82ecd8c80c09cfa
0xPierre <0xpierre@protonmail.com>
2025-10-19 20:38:25 +0200

    chore: add color logging

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,7 @@
 from src.settings import SETTINGS
 import logging.config
 import os
+import colorlog
 
 # Make sure the logs directory exists
 os.makedirs(os.path.dirname(SETTINGS.LOG_PATH), exist_ok=True)
@@ -13,6 +14,33 @@ logging.config.fileConfig(
 
 root_logger = logging.getLogger()
 root_logger.setLevel(SETTINGS.LOG_LEVEL)
+
+# Default format if nothing found
+fmt = "%(asctime)s %(levelname)s %(name)s %(message)s"
+# Remove only Console handler
+for handler in root_logger.handlers:
+    if not isinstance(handler, logging.FileHandler) and isinstance(
+        handler, logging.StreamHandler
+    ):
+        fmt = handler.formatter._fmt
+        root_logger.removeHandler(handler)
+
+ch = logging.StreamHandler()
+ch.setLevel(SETTINGS.LOG_LEVEL)
+
+formatter = colorlog.ColoredFormatter(
+    "%(log_color)s" + fmt,
+    log_colors={
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "red,bg_white",
+    },
+)
+
+ch.setFormatter(formatter)
+root_logger.addHandler(ch)
 
 
 def blank_line_module(log_level="DEBUG", how_many_lines=1):
